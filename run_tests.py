c370a102117ccc891c453363a807c4036188560d
Ryan Lothian <github@ryanlothian.com>
2012-07-18 23:54:45 +0100

    Add copyright headers that were missing. Pre-emptively add a test for wordlist mode (which isn't implemented yet).

--- run_tests.py
+++ run_tests.py
@@ -3,6 +3,7 @@
 import sys
 import os
 import os.path
+import time
 
 red   = "\033[01;31m"
 green = "\033[01;32m"
@@ -11,7 +12,7 @@ off   = "\033[0m"
 SUCCESS = green  + "SUCCESS" + off + "\n"
 FAILURE = red    + "FAILURE" + off + "\n"
 
-def test_against_log(test_id, test_desc, prog, rc_ok=True, sortuniq=False):
+def test_against_log(test_id, test_desc, prog, rc_ok=True, sortuniq=False, max_time=None):
     print "TEST {}: {}:".format(test_id, test_desc),
     sys.stdout.flush()
 
@@ -23,27 +24,36 @@ def test_against_log(test_id, test_desc, prog, rc_ok=True, sortuniq=False):
     else:
         postfix = ""
 
+    start_time = time.time()
     rc = os.system("{} > {}{}".format(prog, run_log, postfix))
+    run_time = time.time() - start_time
+
+    if max_time is not None and run_time > max_time:
+        print FAILURE + " max time {} exceeded: run time was {}".format(max_time, run_time)
+        return False
 
     with open(run_log, "r") as f:
         seen_output = f.read().strip()
 
     if not os.path.exists(good_log):   
-        print FAILURE + " writing log for first time"
+        print "log not present " + FAILURE
         with open(good_log, "w") as f:
             f.write(seen_output)
-        return
+        return False
  
     with open(good_log, "r") as f:
         correct_output = f.read().strip()
     
     if (rc == 0) != rc_ok:
-        print FAILURE, " (rc was {})".format(rc)
+        print "(rc was {}) ".format(rc) + FAILURE
+        return False
     elif seen_output != correct_output:
         print "See {} vs. {} ".format(run_log, good_log) + FAILURE
+        return False
     else:
         print SUCCESS
         os.unlink(run_log)
+        return True
     
 
 def test1():
@@ -78,22 +88,29 @@ def test6():
                      test_desc="cipher implementation test", 
                      prog="bin/test_crack_zip")
 
+def test7():
+    test_against_log(test_id=7,
+                     test_desc="read noradi.zip, wordlist mode", 
+                     prog="echo 'noradi' | bin/test_read_zip test/noradi.zip -w -",
+                     max_time=0.1)
+
+
 def testC1():
     print "TEST c1: check all files have a copyright header",
 
     fmc_fn = "files_missing_copyright"
     os.system("find src/ -regextype posix-extended -regex '.*\.(h|cpp)' | xargs grep -L Copyright > " + fmc_fn)
 
-
     with open(fmc_fn, "r") as f:
-        fmc = f.read().strip().split("\n")
-
+        fmc = [l for l in f.read().strip().split("\n") if l]
+    
     if fmc:
         print FAILURE + "\nThe following files are missing copyright headers:"
         print "\n".join("  * " + line for line in fmc)
     else:
         print SUCCESS
-    os.unlink(fmc_fn)
+        os.unlink(fmc_fn)
+
 
 print ""
 print "Running tests..."
@@ -105,6 +122,7 @@ test3()
 test4()
 test5()
 test6()
+test7()
 
 print "Running coding standards tests..."
 print "---------------------------------"        
--- src/zip_crack.h
+++ src/zip_crack.h
@@ -1,3 +1,23 @@
+/*
+ * rlcrackzip - Zip password cracker.
+ *
+ * Copyright (C) 2012 Ryan Lothian and Marc Lehmann (see AUTHORS file).
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
 #ifndef ZIP_CRACK_H
 #define ZIP_CRACK_H
 
